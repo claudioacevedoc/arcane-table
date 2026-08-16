c45335d6ca8dbcdbc836df6e8421542b2723289d
claudioacevedoc <claudio.acevedo.c@gmail.com>
2026-08-16 15:25:13 -0400

    Refresh PWA cache for v0.6

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const SHELL = 'arcane-shell-v0.5.0';
+const SHELL = 'arcane-shell-v0.6.0';
 const IMAGES = 'arcane-card-images-v0.2.0';
-const ASSETS = ['./','./index.html','./styles.css','./v03.css','./v04.css','./v05.css','./app.js','./v03.js','./v04.js','./v05.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const ASSETS = ['./','./index.html','./styles.css','./v03.css','./v04.css','./v05.css','./v06.css','./app.js','./v03.js','./v04.js','./v05.js','./v06.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(SHELL).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
